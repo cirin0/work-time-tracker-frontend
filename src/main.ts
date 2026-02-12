0175461ee2dd1a2af9cc56d1aa1cd58a237c84ed
cirin0 <ivan.gruziv@gmail.com>
2026-02-12 13:00:59 +0200

    feat: enhance chat functionality by integrating current user profile loading and VueScan support

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { router } from './router'
+import VueScan, { type VueScanOptions } from 'z-vue-scan'
+
+const isProduction = import.meta.env.PROD
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+if (!isProduction) {
+  app.use<VueScanOptions>(VueScan, {})
+}
 
 app.mount('#app')
